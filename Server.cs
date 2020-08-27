0030ef029f49cdfff338c7ff2b586c509af5a4c1
Yunus Emre <callahan_555@hotmail.com>
2020-08-27 16:58:02 +0300

    Removed Prefix byte array and baked it into main queries. Also, removed ConfigureAwait methods to prevent UI applications getting fucked up. And a bit refactoring.

--- Server.cs
+++ Server.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -23,10 +22,9 @@ namespace SteamQuery
         private readonly UdpClient _udpClient;
         private readonly IPEndPoint _ipEndPoint;
 
-        private static readonly byte[] Prefix = { 0xFF, 0xFF, 0xFF, 0xFF }; // Every query starts with this.
-        private static readonly byte[] Informations = { 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00 };
-        private static readonly byte[] Players = { 0x55, 0xFF, 0xFF, 0xFF, 0xFF };
-        private static readonly byte[] Rules = { 0x56, 0xFF, 0xFF, 0xFF, 0xFF };
+        private static readonly byte[] Informations = { 0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00 };
+        private static readonly byte[] Players = { 0xFF, 0xFF, 0xFF, 0xFF, 0x55, 0xFF, 0xFF, 0xFF, 0xFF };
+        private static readonly byte[] Rules = { 0xFF, 0xFF, 0xFF, 0xFF, 0x56, 0xFF, 0xFF, 0xFF, 0xFF };
 
         public Server()
         {
@@ -73,7 +71,7 @@ namespace SteamQuery
                 throw new InvalidPortException();
             }
 
-            await _udpClient.Client.ConnectAsync(_ipEndPoint).ConfigureAwait(false);
+            await _udpClient.Client.ConnectAsync(_ipEndPoint);
 
             // This will return true even if it's not connected properly.
             // Will make custom check later.
@@ -86,23 +84,23 @@ namespace SteamQuery
 
         public async Task<List<Rule>> GetRulesAsync() => ResponseParser.ParseRules(await ExecuteQueryAsync(Rules));
 
-        private async Task<byte[]> ExecuteQueryAsync(IReadOnlyList<byte> rawQuery)
+        private async Task<byte[]> ExecuteQueryAsync(byte[] query)
         {
-            var query = Prefix.Concat(rawQuery).ToArray();
+            await _udpClient.SendAsync(query, query.Length);
+            var result = await _udpClient.ReceiveAsync();
 
-            await _udpClient.SendAsync(query, query.Length).ConfigureAwait(false);
-            var response = _udpClient.ReceiveAsync().ConfigureAwait(false).GetAwaiter().GetResult().Buffer;
+            var response = result.Buffer;
 
-            switch (rawQuery[0])
+            switch (query[0])
             {
-                case 0x54:
+                case 0x54: // Informations
                     return response;
-                case 0x55:
-                case 0x56:
-                    response[4] = rawQuery[0];
+                case 0x55: // Players
+                case 0x56: // Rules
+                    response[4] = query[0];
                     break;
                 default:
-                    throw new UnexpectedByteException(rawQuery[0], 0x54, 0x55, 0x56);
+                    throw new UnexpectedByteException(query[0], 0x54, 0x55, 0x56);
             }
 
             var index = 5;
@@ -110,9 +108,10 @@ namespace SteamQuery
             {
                 response[i] = response.ReadByte(ref index);
             }
-            
-            await _udpClient.SendAsync(response, response.Length).ConfigureAwait(false);
-            return _udpClient.ReceiveAsync().ConfigureAwait(false).GetAwaiter().GetResult().Buffer;
+
+            await _udpClient.SendAsync(response, response.Length);
+            var secondResult = await _udpClient.ReceiveAsync();
+            return secondResult.Buffer;
         }
 
         // Check DisconnectAsync later.
