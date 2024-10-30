bdf0eff25eb5b9bf9b008017fef681c87c1f5ee8
Yunus Emre <callahan_555@hotmail.com>
2024-10-30 11:00:55 +0300

    Fixed ReadNullTerminatedString and excluded null characters from the string.

--- SteamQuery.NET/Extensions/BinaryReaderExtensions.cs
+++ SteamQuery.NET/Extensions/BinaryReaderExtensions.cs
@@ -7,20 +7,25 @@ internal static class BinaryReaderExtensions
 {
     internal static string ReadNullTerminatedString(this BinaryReader reader)
     {
-        var buffer = ArrayPool<byte>.Shared.Rent(1 * 1024);
+        // Steam uses a packet size of up to 1400 bytes + IP/UDP headers. If a request or response needs more packets for the data it starts the packets with an additional header.
+        // So, if you even assume that the whole packet is a one big null terminated string, you'll be safe using 4 KBs of buffer.
+        // But, Rust has been observed breaking this, sending packets up to the maximum IP size - 64KB.
+        var buffer = ArrayPool<byte>.Shared.Rent(64 * 1024);
 
+        var stringLength = buffer.Length;
         for (var i = 0; i < buffer.Length; i++)
         {
             var @byte = reader.ReadByte();
             if (@byte == '\0')
             {
+                stringLength = i;
                 break;
             }
 
             buffer[i] = @byte;
         }
 
-        var @string = Encoding.UTF8.GetString(buffer);
+        var @string = Encoding.UTF8.GetString(buffer, 0, stringLength);
 
         ArrayPool<byte>.Shared.Return(buffer, true);
 
